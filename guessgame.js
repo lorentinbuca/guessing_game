2f89227681f8b355050d2b7beaab6cb32077249f
lorentinbuca <lorentinb@hotmail.com>
2015-10-28 23:59:11 -0700

    First Project

--- guessgame.js
+++ guessgame.js
@@ -29,18 +29,37 @@ var country = prompt("Is China my favorite country to visit? Yes or No");
 		alert("Wrong, my favorite country is Albania.");
 		console.log("Incorrect, my favorite country to visit is Albania:" + country);
 } 
+var city = prompt("Is NYC my favorite city? Yes or No");
+	if  (city.toLowerCase() === "no" || city.toLowerCase() === "nah"){
+		alert("Correct, my favorite city is Miami");
+		console.log('Correct, my favorite city is: ' + city);
+		score += 1;
+}	else {
+		alert("Wrong, my favorite city is Miami.");
+		console.log("Incorrect, my favorite city to visit is city:" + city);
+ }
+var jokeQ1 = parseInt(prompt("How many programmers does it take to change a light bulb"));
+	while(jokeQ1 !== 0) {
+		alert("Guess between 0-4 " + typeof(jokeQ1));
+		console.log("Answer " + typeof(0));
+   	jokeQ1 = parseInt(prompt("Guess again, not the right number."));
+ }
+ 	if(jokeQ1 === 0){
+ 		alert("0 is right, that's a HARDWARE problem!");
+ 		console.log("Good guess.");
+ 		score += 1;
+ }
 var userNumber = parseInt(prompt("What's my favorite number between 1-6"));
 	while(userNumber !== 2) {
 		alert("Guess " + typeof(userNumber));
 		console.log("Answer " + typeof(2));
-   		userNumber = parseInt(prompt("Guess again, not the right number."));
+   	userNumber = parseInt(prompt("Guess again, not the right number."));
  }
- 	if (userNumber === 2) {
- 		
+ 	if(userNumber === 2) {
  		alert("Good guess, you finally got it!");
  		console.log("Good guess.");
  		score += 1;
- 	}
+ }
 
 
-		alert("You answered " + score + " out 4 correct");
+		alert("You answered " + score + " out 6 correct");
